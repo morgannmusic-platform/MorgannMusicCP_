e0141408103e531b72820ba14b9c812699057f11
morgannmusic-platform <morgann.rachedi@icloud.com>
2026-05-20 23:11:24 +0200

    Changement du logo Stripe

--- assets/js/paginateReleases.js
+++
@@ -1,8 +0,0 @@
-// Pagination JS pour catalogue/sorties.html
-export function paginateReleases(releases, pageSize) {
-  const pages = [];
-  for (let i = 0; i < releases.length; i += pageSize) {
-    pages.push(releases.slice(i, i + pageSize));
-  }
-  return pages;
-}
--- service-worker.js
+++
@@ -1,7 +0,0 @@
-// Service Worker minimal pour éviter l'erreur 404
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(event) {
-  self.clients.claim();
-});
